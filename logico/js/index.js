db41f2135997f95abddb631e9985a88d15da4ca2
NextCodigos <nebulanemesis376@gmail.com>
2024-05-27 23:41:35 +0200

    Actividad con condicionales y operadores logicos

---
+++ logico/js/index.js
@@ -0,0 +1,11 @@
+/* valores correctos:
+brendan eich (creador de JavaScript)
+Ha trabajado en las empresas: netscape y brave
+*/
+
+const NOMBRE="brendan";
+const APELLIDO="eich";
+const EMPRESA1="netscape";
+const EMPRESA2="brave";
+
+// Si el nombre escrito por el usuario es "brendan", el apellido es "eich" y ha trabajado en "netscape" o en "brave": escribe "Hola" y sino "Adios".
